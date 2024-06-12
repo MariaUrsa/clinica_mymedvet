06a983ae422dc22950eb428afb63877c5cd976dd
MariaUrsa <129543286+MariaUrsa@users.noreply.github.com>
2024-06-12 22:36:39 +0300

    Add files via upload

---
+++ mymedvet/master/config.rb
@@ -0,0 +1,23 @@
+
+http_path = "/"
+
+css_dir = "../customstyle/css"
+
+sass_dir = "sass"
+
+images_dir = "../customstyle/images"
+
+javascripts_dir = "../customstyle/js"
+
+line_comments = false
+# preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+output_style = :expanded
+# enable relative paths to customstyle via compass helper functions.
+#Uncomment:
+# relative_customstyle = true
+
+# disable debugging comments that display the original location of the selectors. 
+#Uncomment:
+# line_comments = false
